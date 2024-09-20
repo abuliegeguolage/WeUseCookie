2cb43cd8e66ab284a7d8c19e8cf357f893c51e93
abuliegeguolage <123>
2024-09-20 23:06:09 +0800

    feat: FAQ

---
+++ src/pages/FAQ.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import PageTitle from '../components/PageTitle.vue';
+
+const faqs = await fetch('/json/faq.json').then(res => res.json());
+
+</script>
+
+<template>
+    <PageTitle>
+        常見問題
+    </PageTitle>
+
+    <v-expansion-panels variant="inset">
+        <v-expansion-panel v-for="faq in faqs" :title="faq.question" :text="faq.answer">
+        </v-expansion-panel>
+    </v-expansion-panels>
+</template>
+
+<style scoped lang="scss"></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,14 @@ import Home from '../pages/Home.vue';
 import Cookies from '../pages/Cookies.vue';
 import News from '../pages/News.vue';
 import NewsSolo from '../pages/NewsSolo.vue';
+import FAQ from '../pages/FAQ.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/cookies', component: Cookies },
     { path: '/news', component: News },
-    { path: '/news-solo/:id', component: NewsSolo }
+    { path: '/news-solo/:id', component: NewsSolo },
+    { path: '/faq', component: FAQ }
 ]
 
 export const router = createRouter({
